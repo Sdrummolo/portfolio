e73747dadca4293ee73fd96d61fa729e19ce46ad
Luigi Di Paolo <luigidipaolo@Luigis-MacBook-Pro.local>
2020-03-05 11:57:19 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from flask_mail import Mail, Message
+from os import environ
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+app.config['TESTING'] = False
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True 
+app.config['MAIL_USERNAME'] = environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = environ.get('MAIL_USERNAME')
+app.config['MAIL_MAX_EMAILS'] = None
+app.config['MAIL_ASCII_ATTACHMENTS'] = False
+
+mail = Mail(app)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+   if request.method == "POST":
+      # Extract data from form
+      name = request.form.get("name")
+      email = request.form.get("email")
+      message = request.form.get("message")
+
+
+      # Send formatted email
+      msg = Message("New message from portfolio submission!", recipients=[environ.get('MAIL_USERNAME')])
+      msg.html = f"<b>Name:</b> {name}<br><b>Email:</b> {email}<br><br>{message}"
+      mail.send(msg)
+      
+   return render_template("index.html")
---
+++ static/app.js
@@ -0,0 +1,26 @@
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const popup = document.querySelector("#popup");
+const clear = document.querySelector("#clear");
+
+// removes popup
+clear.addEventListener("click", () => {
+	popup.style.display = "none";
+});
+
+document.addEventListener("submit", e => {
+	const form = e.target;
+
+	fetch(form.action, {
+		method: form.method,
+		body: new FormData(form)
+	}).then(data => {
+		popup.style.display = "flex";
+		name.value = "";
+		email.value = "";
+		message.value = "";
+	});
+
+	e.preventDefault();
+});
