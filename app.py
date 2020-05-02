69ee1888ff4a8e54027d7b15949ac1bff7d92095
Sdrummolo <52413900+Sdrummolo@users.noreply.github.com>
2020-05-02 09:36:57 +0100

    updated skills section, added tweeetter

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config['TESTING'] = False
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True 
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = environ.get('MAIL_USERNAME')
@@ -18,18 +18,19 @@ app.config['MAIL_ASCII_ATTACHMENTS'] = False
 
 mail = Mail(app)
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-   if request.method == "POST":
-      # Extract data from form
-      name = request.form.get("name")
-      email = request.form.get("email")
-      message = request.form.get("message")
+    if request.method == "POST":
+        # Extract data from form
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
 
+        # Send formatted email
+        msg = Message("New message from portfolio submission!",
+                      recipients=[environ.get('MAIL_USERNAME')])
+        msg.html = f"<b>Name:</b> {name}<br><b>Email:</b> {email}<br><br>{message}"
+        mail.send(msg)
 
-      # Send formatted email
-      msg = Message("New message from portfolio submission!", recipients=[environ.get('MAIL_USERNAME')])
-      msg.html = f"<b>Name:</b> {name}<br><b>Email:</b> {email}<br><br>{message}"
-      mail.send(msg)
-      
-   return render_template("index.html")
+    return render_template("index.html")
