7af8d4867dd7ca6bc57423d6bc42ae6bb4925f56
Sdrummolo <52413900+Sdrummolo@users.noreply.github.com>
2020-05-05 09:59:13 +0100

    Fixed indentation issue

--- static/app.js
+++ static/app.js
@@ -1,26 +1,26 @@
-const name = document.querySelector("#name");
-const email = document.querySelector("#email");
-const message = document.querySelector("#message");
-const popup = document.querySelector("#popup");
-const clear = document.querySelector("#clear");
+const name = document.querySelector("#name")
+const email = document.querySelector("#email")
+const message = document.querySelector("#message")
+const popup = document.querySelector("#popup")
+const clear = document.querySelector("#clear")
 
 // removes popup
 clear.addEventListener("click", () => {
-	popup.style.display = "none";
-});
+   popup.style.display = "none"
+})
 
-document.addEventListener("submit", e => {
-	const form = e.target;
+document.addEventListener("submit", (e) => {
+   const form = e.target
 
-	fetch(form.action, {
-		method: form.method,
-		body: new FormData(form)
-	}).then(data => {
-		popup.style.display = "flex";
-		name.value = "";
-		email.value = "";
-		message.value = "";
-	});
+   fetch(form.action, {
+      method: form.method,
+      body: new FormData(form),
+   }).then((data) => {
+      popup.style.display = "flex"
+      name.value = ""
+      email.value = ""
+      message.value = ""
+   })
 
-	e.preventDefault();
-});
+   e.preventDefault()
+})
